4e844bb7314019ab07b64f96bfa3c912bef57602
kingkite <kitewebdevelopers@gmail.com>
2023-07-25 22:37:21 +0100

    Using react table for showing tables

--- src/components/table/cells.jsx
+++ src/components/table/cells.jsx
@@ -1,33 +1,29 @@
-export function TableHeadCell({ title }) {
-	return (
-		<span className="font-medium text-gray-700 text-sm md:text-base">
-			{title}
-		</span>
-	);
-}
-
-export function TableAvatarTitleOtherCell({ other, image, title }) {
+export function TableAvatarTitleSubCell({ column, row }) {
 	return (
 		<div className="flex items-center py-2">
-			{image ? (
+			{row.original[column.avatarAccessor] ? (
 				<section className="flex-shrink-0 h-10 w-10">
 					<div className="h-10 relative rounded-full w-10">
-						<img alt={title[0]} className="rounded-full" src={image} />
+						<img
+							alt={row.original[column.titleAccessor][0]}
+							className="rounded-full"
+							src={row.original[column.avatarAccessor]}
+						/>
 					</div>
 				</section>
 			) : (
 				<span className="bg-primary-500 h-10 inline-flex items-center justify-center rounded-full text-gray-50 w-10">
 					<span className="left-[0.05rem] relative top-[0.075rem]">
-						{title[0]}
+						{row.original[column.titleAccessor][0]}
 					</span>
 				</span>
 			)}
 			<section className="ml-4 text-left">
 				<div className="text-sm font-medium text-gray-700 md:text-base">
-					{title}
+					{row.original[column.titleAccessor]}
 				</div>
 				<div className="normal-case font-normal text-sm text-gray-500">
-					{other}
+					{row.original[column.subAccessor]}
 				</div>
 			</section>
 		</div>
--- src/containers/dashboard/patients/index.jsx
+++ src/containers/dashboard/patients/index.jsx
@@ -4,45 +4,41 @@ import {
 	EditOutlined,
 	DeleteOutlined,
 } from '@ant-design/icons';
-import { Button, Input, Table, Tooltip } from 'antd';
+import { Button, Tooltip } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Container from '../../../components/container';
-import {
-	TableAvatarTitleOtherCell as PatientCell,
-	TableHeadCell,
-} from '../../../components/table/cells';
+import { TableAvatarTitleSubCell as PatientCell } from '../../../components/table/cells';
+import ReactTable from '../../../components/table';
 import routes from '../../../config/routes';
 
 function Patients() {
 	const columns = React.useMemo(
 		() => [
 			{
-				className: 'font-normal text-gray-700 text-sm',
-				title: <TableHeadCell title="Patient" />,
-				dataIndex: 'patient',
-				width: '40%',
+				Cell: PatientCell,
+				Header: 'Patient',
+				accessor: 'patient',
+				avatarAccessor: 'avatar',
+				subAccessor: 'email',
+				titleAccessor: 'full_name',
 			},
 			{
-				className: 'font-normal text-gray-700 text-sm',
-				title: <TableHeadCell title="Phone number" />,
-				dataIndex: 'phone',
+				Header: 'Phone number',
+				accessor: 'phone',
 			},
 			{
-				className: 'font-normal text-gray-700 text-sm',
-				title: <TableHeadCell title="Gender" />,
-				dataIndex: 'gender',
+				Header: 'Gender',
+				accessor: 'gender',
 			},
 			{
-				className: 'font-normal text-gray-700 text-sm',
-				title: <TableHeadCell title="Date of Birth" />,
-				dataIndex: 'dob',
+				Header: 'Date of Birth',
+				accessor: 'dob',
 			},
 			{
-				className: 'font-normal text-gray-700 text-sm',
-				title: <TableHeadCell title="Actions" />,
-				dataIndex: 'actions',
+				Header: 'Actions',
+				accessor: 'actions',
 			},
 		],
 		[]
@@ -85,13 +81,8 @@ function Patients() {
 		];
 		const keyedData = data.map((item, index) => ({
 			...item,
-			patient: (
-				<PatientCell
-					other={item.email}
-					title={item.first_name + ' ' + item.last_name}
-				/>
-			),
 			key: index,
+			full_name: item.first_name + ' ' + item.last_name,
 			actions: (
 				<div className="flex items-center">
 					<span className="px-2">
@@ -131,61 +122,24 @@ function Patients() {
 		return keyedData;
 	}, []);
 
-	const onChange = React.useCallback((pagination, filters, sorter, extra) => {
-		console.log('params', pagination, filters, sorter, extra);
-	}, []);
-
 	return (
 		<Container title="Patients">
-			<div className="gap-4 grid grid-cols-1 items-end my-4 sm:grid-cols-3 lg:grid-cols-4">
-				<div className="sm:col-span-2">
-					<label
-						className="block font-medium my-1 text-sm text-gray-700 md:text-base"
-						htmlFor="search"
-					>
-						Search
-					</label>
-					<Input.Search
-						allowClear
-						className="text-sm lg:text-base"
-						id="search"
-						name="search"
-						// disabled={loading}
-						placeholder="Search for patients e.g. Richard Cooper"
+			<div className="w-full sm:w-1/2 md:w-1/3">
+				<Link className="block w-full" to={routes.PATIENT_CREATE_PAGE}>
+					<Button
+						block
+						icon={
+							<span className="mr-2 text-sm md:text-base">
+								<PlusOutlined />
+							</span>
+						}
 						size="large"
-						type="search"
-					/>
-				</div>
-				<div className="sm:col-span-1">
-					<Link className="block w-full" to={routes.PATIENT_CREATE_PAGE}>
-						<Button
-							block
-							icon={
-								<span className="mr-2 text-sm md:text-base">
-									<PlusOutlined />
-								</span>
-							}
-							size="large"
-						>
-							<span className="text-sm md:text-base">New Patient</span>
-						</Button>
-					</Link>
-				</div>
+					>
+						<span className="text-sm md:text-base">New Patient</span>
+					</Button>
+				</Link>
 			</div>
-			<Table
-				columns={columns}
-				dataSource={data}
-				loading={false}
-				onChange={onChange}
-				pagination={{
-					pageSize: 50,
-				}}
-				rowClassName=""
-				size="large"
-				scroll={{
-					y: 350,
-				}}
-			/>
+			<ReactTable columns={columns} data={data} />
 		</Container>
 	);
 }
