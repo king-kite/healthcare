211825e4bfd031356ffd30a3b6749cbd299d8ed1
kingkite <kitewebdevelopers@gmail.com>
2023-07-25 20:52:50 +0100

    Changed the design a bit

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import routes from '../config/routes';
 
 function Page() {
+	const navigate = useNavigate();
+
 	return (
 		<main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
 			<div className="text-center">
@@ -13,12 +15,18 @@ function Page() {
 				<p className="mt-6 text-base leading-7 text-gray-600">
 					Sorry, we couldn&apos;t find the page you&apos;re looking for.
 				</p>
-				<div className="gap-6 mt-10 flex flex-col items-center justify-center sm:flex-row">
+				<div className="gap-6 mt-10 flex flex-col items-center justify-between sm:flex-row">
+					<span
+						onClick={() => navigate(-1)}
+						className="rounded-md no-underline bg-primary-600 mx-2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+					>
+						Go back
+					</span>
 					<Link
 						to={routes.HOME_PAGE}
-						className="rounded-md no-underline bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+						className="rounded-md no-underline bg-primary-600 mx-2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
 					>
-						Go back home
+						Go home
 					</Link>
 				</div>
 			</div>
