8c672c90071aeb4073cf09ce1df4f5a019f4161b
kingkite <kitewebdevelopers@gmail.com>
2023-07-27 20:35:07 +0100

    Updated the patient validator

--- src/validators/patient.js
+++ src/validators/patient.js
@@ -1,12 +1,14 @@
-import { date, object, string } from 'yup';
+import { date, object, string } from "yup";
 
 const schema = object({
-	first_name: string().required().label('First Name'),
-	last_name: string().required().label('Last Name'),
-	email: string().email().required().label('Email Address'),
-	dob: date().required().label('Date of Birth'),
-	gender: string().oneOf(['Male', 'Female']).required().label('Gender'),
-	phone: string().required().label('Phone Number'),
+	first_name: string().required().label("First Name"),
+	last_name: string().required().label("Last Name"),
+	email: string().email().required().label("Email Address"),
+	dob: date().required().label("Date of Birth"),
+	gender: string().oneOf(["Male", "Female"]).required().label("Gender"),
+	phone: string().required().label("Phone Number"),
+	image: string().label("Image"),
+	image_ref: string().label("Image Reference"),
 });
 
 export default schema;
