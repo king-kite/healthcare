9de8f79cd4dca18d768aca038cd56d8ea3ffaf07
kingkite <kitewebdevelopers@gmail.com>
2023-07-28 23:01:49 +0100

    Added image validation to patients

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,17 +1,22 @@
-import { ValidationError } from 'yup';
+import { ValidationError } from "yup";
 
 export function handleYupError(err) {
 	if (err instanceof ValidationError) {
 		if (err.inner.length > 0) {
-			let error = null;
-			err.inner.forEach((error) => {
-				const { message } = error;
-				if (!error) error = message;
-			});
-			return { message: error };
+			const error = err.inner.reduce(
+				(acc, item) => {
+					if (acc.message !== null) return acc;
+					return {
+						message: item.message,
+						path: item.path,
+					};
+				},
+				{ message: null, path: null }
+			);
+			return error;
 		} else {
 			const error = err.errors[0] || err.message;
-			return { message: error };
+			return { message: error, path: err.path };
 		}
 	}
 	return undefined;
