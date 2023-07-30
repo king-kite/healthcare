8456c03fffd0f325fcff0225213bc7d30e7d9b3c
kingkite <kitewebdevelopers@gmail.com>
2023-07-30 05:38:35 +0100

    Showing the correct number of patients and tests

--- src/components/dashboard/cards.jsx
+++ src/components/dashboard/cards.jsx
@@ -1,41 +1,43 @@
 import { GroupOutlined, SolutionOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 const cards = [
 	{
 		color: 'text-primary-500',
 		icon: GroupOutlined,
 		title: 'No. of Patients',
-		value: '228',
+		key: 'patients',
 	},
 	{
 		color: 'text-red-600',
 		icon: SolutionOutlined,
 		title: 'Total Tests/Diagnosis',
-		value: '1023',
+		key: 'tests',
 	},
 ];
 
-function Cards() {
+function Cards({ loading, values }) {
 	return (
 		<div className="gap-4 grid grid-cols-1  my-4 sm:gap-5 md:gap-6 sm:grid-cols-2 lg:gap-8">
-			{cards.map(({ color, icon: Icon, title, value }, index) => (
-				<div
-					className="bg-white flex items-center justify-between p-4 rounded-md shadow-lg md:p-5 lg:p-6"
-					key={index}
-				>
-					<div className="pr-2">
-						<h1
-							className={`${color} font-bold mb-2 text-gray-700 text-2xl sm:text-3xl`}
-						>
-							{value}
-						</h1>
-						<p className="text-gray-700 text-sm md:text-base">{title}</p>
-					</div>
-					<div className="pl-2">
-						<span className={`${color} text-3xl`}>
-							<Icon />
-						</span>
-					</div>
+			{cards.map(({ color, icon: Icon, title, key }, index) => (
+				<div className="h-full w-full" key={index}>
+					<Spin size="small" spinning={loading[key]}>
+						<div className="bg-white flex items-center justify-between p-4 rounded-md shadow-lg md:p-5 lg:p-6">
+							<div className="pr-2">
+								<h1
+									className={`${color} font-bold mb-2 text-gray-700 text-2xl sm:text-3xl`}
+								>
+									{values[key]}
+								</h1>
+								<p className="text-gray-700 text-sm md:text-base">{title}</p>
+							</div>
+							<div className="pl-2">
+								<span className={`${color} text-3xl`}>
+									<Icon />
+								</span>
+							</div>
+						</div>
+					</Spin>
 				</div>
 			))}
 		</div>
--- src/containers/dashboard/index.jsx
+++ src/containers/dashboard/index.jsx
@@ -2,11 +2,25 @@ import Container from '../../components/container';
 import Cards from '../../components/dashboard/cards';
 import PatientTable from '../../components/dashboard/patient-table';
 import TestTable from '../../components/dashboard/test-table';
+import { useGetPatientsQuery } from '../../store/features/api/patients';
+import { useGetTestsQuery } from '../../store/features/api/tests';
 
 function Dashboard() {
+	const { data: patients, isLoading: patientsLoading } = useGetPatientsQuery();
+	const { data: tests, isLoading: testsLoading } = useGetTestsQuery();
+
 	return (
 		<Container title="Overview">
-			<Cards />
+			<Cards
+				loading={{
+					patients: patientsLoading,
+					tests: testsLoading,
+				}}
+				values={{
+					patients: patients ? patients.length : '----',
+					tests: tests ? tests.length : '----',
+				}}
+			/>
 			<PatientTable />
 			<TestTable />
 		</Container>
