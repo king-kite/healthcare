868f544bc49d26975c3662817e35febcea622ae1
kingkite <kitewebdevelopers@gmail.com>
2023-07-30 05:11:32 +0100

    Using the container component

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,18 +1,47 @@
-import { Alert } from "antd";
+import { Alert } from 'antd';
 
-function Container({ children, alert, title }) {
+function Container({ children, alert, image, title = 'Page' }) {
 	return (
 		<>
 			<div className="container-banner">
 				<div className="wrapper">
-					<div className="container-dashboard flex h-full items-end p-4 relative w-full">
-						<h2 className="text-2xl font-bold leading-7 text-gray-100 sm:text-3xl sm:tracking-tight">
+					<div
+						className={`${
+							image ? 'sm:items-end' : 'items-end'
+						} container-dashboard flex h-full p-4 relative w-full`}
+					>
+						<h2
+							className={`${
+								image ? 'sm:ml-28 lg:ml-24' : ''
+							} text-2xl font-bold leading-7 text-gray-100 sm:text-3xl sm:tracking-tight`}
+						>
 							{title}
 						</h2>
+						{image && (
+							<span className="absolute bottom-[-3rem] left-4">
+								<span className="bg-primary-500 border-4 border-gray-100 border-solid h-24 inline-flex items-center justify-center relative rounded-full text-gray-50 w-24">
+									{image.url ? (
+										<img
+											className="h-full rounded-full w-full"
+											src={image.url}
+											alt={image.alt}
+										/>
+									) : (
+										<span className="left-[0.05rem] relative text-xl top-[0.075rem]">
+											{image.alt || title[0]}
+										</span>
+									)}
+								</span>
+							</span>
+						)}
 					</div>
 				</div>
 			</div>
-			<div className="container-dashboard py-4">
+			<div
+				className={`container-dashboard ${
+					image ? 'pt-16 pb-4 sm:pt-20' : 'py-4'
+				}`}
+			>
 				{alert && (
 					<div className="my-3">
 						<Alert
--- src/containers/dashboard/tests/detail.jsx
+++ src/containers/dashboard/tests/detail.jsx
@@ -4,6 +4,7 @@ import { Spin } from 'antd';
 import React from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
+import Container from '../../../components/container';
 import DeleteTest from '../../../components/tests/delete-test';
 import routes from '../../../config/routes';
 import { useGetTestQuery } from '../../../store/features/api/tests';
@@ -81,92 +82,61 @@ function Detail() {
 	if (!data) return <Navigate to="/404" />;
 
 	return (
-		<>
-			<div className="profile-top-image">
-				<div className="wrapper">
-					<div className="container-dashboard h-full mx-auto relative w-full">
-						<span className="absolute bottom-[-3rem] left-4">
-							<span className="bg-primary-500 border-4 border-gray-100 border-solid h-24 inline-flex items-center justify-center relative rounded-full text-gray-50 w-24">
-								{data.patient?.image ? (
-									<img
-										className="h-full rounded-full w-full"
-										src={data.patient.image}
-										alt={
-											data.patient
-												? data.patient.first_name[0] +
-												  ' ' +
-												  data.patient.last_name[0]
-												: '-----'
-										}
-									/>
-								) : (
-									<span className="left-[0.05rem] relative text-xl top-[0.075rem]">
-										{data.patient
-											? data.patient.first_name[0] +
-											  ' ' +
-											  data.patient.last_name[0]
-											: '-----'}
-									</span>
-								)}
+		<Container
+			image={{
+				url: data.patient?.image,
+				alt: data.patient
+					? data.patient.first_name[0] + ' ' + data.patient.last_name[0]
+					: undefined,
+			}}
+			title="Test Information"
+		>
+			<h1 className="font-bold my-2 text-gray-700 text-lg">{data.full_name}</h1>
+			<div className="sm:flex sm:items-center">
+				<div className="my-4 sm:my-0">
+					<DeleteTest
+						className="cursor-pointer"
+						testId={id}
+						shape="default"
+						onSuccess={() => navigate(routes.TESTS_PAGE)}
+						icon={
+							<span className="mr-2 text-gray-700 text-sm md:text-base">
+								<DeleteOutlined />
 							</span>
-						</span>
-					</div>
+						}
+						size="large"
+						type="default"
+					>
+						<span className="text-gray-700 text-sm md:text-base">Delete</span>
+					</DeleteTest>
 				</div>
 			</div>
-			<div className="container-dashboard py-4 w-full">
-				<div className="my-8 py-2">
-					<h1 className="font-bold mb-2 mt-5 text-gray-700 text-lg">
-						{data.full_name}
-					</h1>
-					<div className="sm:flex sm:items-center">
-						<div className="my-4 sm:my-0">
-							<DeleteTest
-								className="cursor-pointer"
-								testId={id}
-								shape="default"
-								onSuccess={() => navigate(routes.TESTS_PAGE)}
-								icon={
-									<span className="mr-2 text-gray-700 text-sm md:text-base">
-										<DeleteOutlined />
-									</span>
-								}
-								size="large"
-								type="default"
-							>
-								<span className="text-gray-700 text-sm md:text-base">
-									Delete
-								</span>
-							</DeleteTest>
-						</div>
-					</div>
-					<hr className="border-gray-100 my-4" />
-					<div>
-						<div>
-							<h3 className="text-base font-semibold leading-7 text-gray-900">
-								Patient&rsquo;s Test/Diagnosis Information
-							</h3>
-							<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-								View test/diagnosis data on this patient.
-							</p>
-						</div>
-						<div className="mt-6 border-t border-gray-100">
-							<dl className="divide-y divide-gray-100 lg:grid lg:grid-cols-2">
-								{info.map((detail, index) => (
-									<div key={index} className="px-4 py-6">
-										<dt className="text-sm font-medium leading-6 text-gray-900 md:text-base md:font-semibold">
-											{detail.title}
-										</dt>
-										<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 md:text-base md:font-medium">
-											{detail.value}
-										</dd>
-									</div>
-								))}
-							</dl>
-						</div>
-					</div>
+			<hr className="border-gray-100 my-4" />
+			<div>
+				<div>
+					<h3 className="text-base font-semibold leading-7 text-gray-900">
+						Patient&rsquo;s Test/Diagnosis Information
+					</h3>
+					<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+						View test/diagnosis data on this patient.
+					</p>
+				</div>
+				<div className="mt-6 border-t border-gray-100">
+					<dl className="divide-y divide-gray-100 lg:grid lg:grid-cols-2">
+						{info.map((detail, index) => (
+							<div key={index} className="px-4 py-6">
+								<dt className="text-sm font-medium leading-6 text-gray-900 md:text-base md:font-semibold">
+									{detail.title}
+								</dt>
+								<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 md:text-base md:font-medium">
+									{detail.value}
+								</dd>
+							</div>
+						))}
+					</dl>
 				</div>
 			</div>
-		</>
+		</Container>
 	);
 }
 
