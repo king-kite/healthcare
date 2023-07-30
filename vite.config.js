4e70073a5a986afee170fb49303fc1e3e689dd03
kingkite <kitewebdevelopers@gmail.com>
2023-07-30 08:40:51 +0100

    Updated the configs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 const port = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	envDir: "../",
+	build: {
+		outDir: '../dist',
+	},
+	envDir: '../',
 	plugins: [react()],
-	publicDir: "../public",
-	root: "./src",
+	publicDir: '../public',
+	root: './src',
 	server: { port },
 });
