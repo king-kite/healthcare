b1435e97c4b08bc53d317b98dd5e78a178b6e045
kingkite <kitewebdevelopers@gmail.com>
2023-08-02 20:46:19 +0100

    Added new field to test validation

--- src/validators/test.js
+++ src/validators/test.js
@@ -1,11 +1,12 @@
-import { object, string } from "yup";
+import { object, number, string } from 'yup';
 
 const schema = object({
-  patient_id: string().required().label("Patient ID"),
-  height: string().required().label("Height"),
-  weight: string().required().label("Weight"),
-  temperature: string().required().label("Temperature"),
-  pulse: string().required().label("Pulse"),
+	activity: number().required().min(1).max(5).label('Activity Level'),
+	patient_id: string().required().label('Patient ID'),
+	height: string().required().label('Height'),
+	weight: string().required().label('Weight'),
+	temperature: string().required().label('Temperature'),
+	pulse: string().required().label('Pulse'),
 });
 
 export default schema;
