c3364b44722f03c3be0e62476445fe70c5b1127b
kingkite <kitewebdevelopers@gmail.com>
2023-08-15 21:54:02 +0100

    Added settings Page

--- src/containers/dashboard/settings.jsx
+++ src/containers/dashboard/settings.jsx
@@ -1,26 +1,27 @@
 import { Switch } from 'antd';
+import React from 'react';
 
 import Container from '../../components/container';
 import { Select } from '../../components/controls';
-import { getSettings } from '../../config/settings';
-
-// // delete patient's tests when the patient is deleted
-// delete_tests_on_patient_delete: 1,
-
-// unit_bmr: 'kcal', // 'kcal | J'
+import { getSettings, changeSetting } from '../../config/settings';
 
 function Settings() {
-	const settings = getSettings();
+	const [settings, setSettings] = React.useState(getSettings);
+
+	const handleSettingsChange = React.useCallback((name, value) => {
+		const newSettings = changeSetting(name, value);
+		setSettings(newSettings);
+	}, []);
 
 	return (
 		<Container title="Settings">
 			<div className="grid gap-6 grid-cols-1 my-3 md:grid-cols-2 md:grid-rows-2">
 				{/* Measurement Units */}
 				<div className="bg-white rounded-lg shadow-lg md:row-span-2">
-					<h3 className="bg-gray-300 px-3 py-1 rounded-t-lg text-gray-700 text-base md:text-lg">
+					<h3 className="bg-gray-200 px-3 py-1 font-semibold uppercase rounded-t-lg text-gray-700 text-sm md:text-base">
 						Measurement Units
 					</h3>
-					<p className="bg-gray-200 px-3 py-1 text-gray-600 text-sm md:text-base">
+					<p className="bg-gray-100 px-3 py-1 text-gray-600 text-sm">
 						Select the unit of measurement of each medical parameter
 					</p>
 					<div className="grid grid-cols-1 gap-4 px-3 py-5">
@@ -28,6 +29,9 @@ function Settings() {
 							<Select
 								label="Height"
 								value={settings.unit_height}
+								onSelect={(value) => {
+									handleSettingsChange('unit_height', value);
+								}}
 								options={[
 									{
 										label: 'Centimeters',
@@ -56,6 +60,9 @@ function Settings() {
 							<Select
 								label="Weight"
 								value={settings.unit_weight}
+								onSelect={(value) => {
+									handleSettingsChange('unit_weight', value);
+								}}
 								options={[
 									{
 										label: 'Grams',
@@ -76,34 +83,40 @@ function Settings() {
 							<Select
 								label="Temperature"
 								value={settings.unit_temperature}
+								onSelect={(value) => {
+									handleSettingsChange('unit_temperature', value);
+								}}
 								options={[
 									{
-										label: 'Celcius',
+										label: 'Celcius (C)',
 										value: 'C',
 									},
 									{
-										label: 'Fahrenheit',
+										label: 'Fahrenheit (F)',
 										value: 'F',
 									},
 									{
-										label: 'Kelvin',
+										label: 'Kelvin (K)',
 										value: 'K',
 									},
 								]}
 							/>
 						</div>
-						{settings.show_bmr && (
+						{settings.show_bmr === 1 && (
 							<div>
 								<Select
 									label="Basal Metabolic Rate"
 									value={settings.unit_bmr}
+									onSelect={(value) => {
+										handleSettingsChange('unit_bmr', value);
+									}}
 									options={[
 										{
-											label: 'Calories',
+											label: 'Calories (kcal)',
 											value: 'kcal',
 										},
 										{
-											label: 'Joules',
+											label: 'Joules (J)',
 											value: 'J',
 										},
 									]}
@@ -115,21 +128,27 @@ function Settings() {
 
 				{/* Other Medical Parameters */}
 				<div className="bg-white rounded-lg shadow-lg md:row-span-1">
-					<h3 className="bg-gray-300 px-3 py-1 rounded-t-lg text-gray-700 text-base md:text-lg">
+					<h3 className="bg-gray-200 px-3 py-1 font-semibold uppercase rounded-t-lg text-gray-700 text-sm md:text-base">
 						Other Medical Parameters
 					</h3>
-					<p className="bg-gray-200 px-3 py-1 text-gray-600 text-sm md:text-base">
-						Show other patient parameters generated fromthe primary ones.
+					<p className="bg-gray-100 px-3 py-1 text-gray-600 text-sm md:text-base">
+						Show other patient parameters generated from the primary ones.
 					</p>
 					<div className="p-3">
 						<div className="flex items-center my-3">
-							<Switch />
+							<Switch
+								checked={settings.show_bmi === 1}
+								onChange={(checked) => handleSettingsChange('show_bmi', checked ? 1 : 0)}
+							/>
 							<p className="font-medium ml-1 px-3 py-1 text-gray-600 text-sm md:text-base">
 								Body Mass Index(BMI)
 							</p>
 						</div>
 						<div className="flex items-center my-3">
-							<Switch />
+							<Switch
+								checked={settings.show_bmr === 1}
+								onChange={(checked) => handleSettingsChange('show_bmr', checked ? 1 : 0)}
+							/>
 							<p className="font-medium ml-1 px-3 py-1 text-gray-600 text-sm md:text-base">
 								Basal Metabolic Rate(BMR)
 							</p>
@@ -139,16 +158,21 @@ function Settings() {
 
 				{/* Records */}
 				<div className="bg-white rounded-lg shadow-lg md:row-span-1">
-					<h3 className="bg-gray-300 px-3 py-1 rounded-t-lg text-gray-700 text-base md:text-lg">
+					<h3 className="bg-gray-200 px-3 py-1 font-semibold rounded-t-lg uppercase text-gray-700 text-sm md:text-base">
 						Records
 					</h3>
-					<p className="bg-gray-200 px-3 py-1 text-gray-600 text-sm md:text-base">
+					<p className="bg-gray-100 px-3 py-1 text-gray-600 text-sm">
 						Change settings that effect record keeping logic.
 					</p>
 					<div className="p-3">
 						<div className="flex items-center my-3">
-							<Switch />
-							<p className="font-medium ml-1 px-3 py-1 text-gray-600 text-sm md:text-base">
+							<Switch
+								checked={settings.delete_tests_on_patient_delete === 1}
+								onChange={(checked) =>
+									handleSettingsChange('delete_tests_on_patient_delete', checked ? 1 : 0)
+								}
+							/>
+							<p className="font-medium ml-1 px-3 py-1 text-gray-600 text-sm">
 								Remove patient&rsquo;s test upon deletion
 							</p>
 						</div>
